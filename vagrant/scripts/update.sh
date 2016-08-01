78edaef623e98ed731b77107c639a04440ac8d49
Johandry Amador <johandry@yahoo.com>
2016-08-01 11:30:47 -0700

    Fix typo

--- vagrant/scripts/update.sh
+++ vagrant/scripts/update.sh
@@ -2,7 +2,7 @@
 
 message () { echo -e "\033[93;1mSCRIPT:\033[0m ${1}"; }
 
-message "Updating pachakes"
+message "Updating packages"
 sudo yum update -y
 
 message "If the update took a lot of time, then it is time to rebuild the box with Packer. Execute ./build.sh"
